6925176fbd810bc771378c94d7a318628e985699
m.baset@zoho.com <m.baset@zoho.com>
2025-02-08 13:42:36 +0200

    updates

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === "production", httpOnly: true, maxAge: null },
+    cookie: { secure: process.env.NODE_ENV === "production", httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 },
   })
 );
 
